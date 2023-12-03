cbedec676f3b9eb9c9dc11acb6a56406ba7c7f12
yijidev <119959530+yijidev@users.noreply.github.com>
2023-12-03 20:27:39 +0900

    Update kubeadm-install.sh
    apt-get > apt

--- kubeadm-install.sh
+++ kubeadm-install.sh
@@ -4,20 +4,20 @@ echo "..................................... 스왑 영구 비활성화"
 sed -i '/swap/s/^/#/' /etc/fstab
 
 echo "..................................... apt 패키지 색인 업데이트, k8s apt repo 패키지 설치"
-apt-get update
-apt-get install -y apt-transport-https ca-certificates
+apt update
+apt install -y apt-transport-https ca-certificates
 
 echo "..................................... 공개 사이닝 키 다운로드"
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
+curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://dl.k8s.io/apt/doc/apt-key.gpg
 sleep 30s
 
 echo "..................................... 쿠버네티스 apt repo 추가"
 echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 echo "..................................... apt 패키지 색인 업데이트, k 설치 및 해당 버전 고정"
-apt-get update
-apt-get install -y kubelet=1.27.5-00 kubeadm=1.27.5-00 kubectl=1.27.5-00
-apt-mark hold kubelet kubeadm kubectl
+apt update
+apt install -y kubelet=1.27.5-00 kubeadm=1.27.5-00 kubectl=1.27.5-00
+apt hold kubelet kubeadm kubectl
 
 echo "..................................... Restart kubelet"
 systemctl daemon-reload
